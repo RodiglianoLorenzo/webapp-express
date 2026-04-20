3c47099f1640ebf622b90626e170a4687d3bef76
Lorenzo Rodigliano <rodiglianolorenzo@gmail.com>
2026-04-20 12:01:36 +0200

    Linked App on database

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+const connection = require('./database/connection');
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server is Started");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Started on http://localhost:${PORT}`);
+});
